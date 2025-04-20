e51858e6050773ac0451e29f8eb57866f78e613b
Prasanna Anbazhagan <prasannaa@sahaj.ai>
2025-04-20 16:51:34 +0100

    add graphql support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
-import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo';
-import {Module} from '@nestjs/common';
-import {GraphQLModule} from '@nestjs/graphql';
-import {UserResolver} from './user/user.resolver';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UserResolver } from './user/user.resolver';
 
 @Module({
-    imports: [
-        GraphQLModule.forRoot<ApolloDriverConfig>({
-            driver: ApolloDriver,
-            playground: true,
-            typePaths: ['./**/*.graphql'],
-        }),
-        UserResolver,
-    ],
-    controllers: [],
-    providers: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+    }),
+    UserResolver,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {
 }
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,12 +1,12 @@
-import {Query, Resolver} from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 
 @Resolver('User')
 export class UserResolver {
-    @Query('user')
-    getUser() {
-        return {
-            name: 'John Doe',
-            email: ''
-        };
-    }
+  @Query('user')
+  getUser() {
+    return {
+      name: 'John Doe',
+      email: '',
+    };
+  }
 }
